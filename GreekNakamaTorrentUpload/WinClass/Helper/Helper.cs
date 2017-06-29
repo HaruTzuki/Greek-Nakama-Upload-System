6d38709158aaad72e257112b1d8433ce212d1c9f
HaruTzuki <HaruTzuki@DESKTOP-32L8FEI>
2017-06-30 01:23:04 +0300

    Helper Class added.

---
+++ GreekNakamaTorrentUpload/WinClass/Helper/Helper.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GreekNakamaTorrentUpload.WinClass.Helper
+{
+    static class Helper
+    {
+        static public void BrowseFiles(TextBox txt_torrent, Label lbl_file)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.Filter = "Torrent files|*.torrent";
+                ofd.Multiselect = false;
+                ofd.Title = "Browse the torrent file...";
+                DialogResult DL = ofd.ShowDialog();
+                if (DL == DialogResult.OK)
+                {
+                    txt_torrent.Text = ofd.FileName;
+                    lbl_file.Text = "File Name : " + ofd.SafeFileName;
+                }
+
+            }
+        }
+
+
+        static public void SaveToJson(string content)
+        {
+            string contentForSave = JsonConvert.SerializeObject(content, Formatting.Indented);
+            File.WriteAllText("settings.json", contentForSave);
+        }
+
+        static public void SaveToJson(string[] contents)
+        {
+            string contentForSave = JsonConvert.SerializeObject(contents.ToArray(), Formatting.Indented);
+            File.WriteAllText("settings.json", contentForSave);
+        }
+
+        static public string LoadFromJson()
+        {
+            string loadedContents;
+            try
+            {
+                loadedContents = JsonConvert.DeserializeObject<string>(System.IO.File.ReadAllText("settings.json"));
+            }
+            catch (Exception)
+            {
+                return "0x01";
+            }
+
+            return loadedContents;
+        }
+    }
+}
