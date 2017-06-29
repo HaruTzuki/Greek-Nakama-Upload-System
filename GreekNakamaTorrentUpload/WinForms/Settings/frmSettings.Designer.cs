6106c88dff26dc44face66db4c8b6b94e5afc195
HaruTzuki <HaruTzuki@DESKTOP-32L8FEI>
2017-06-30 01:20:50 +0300

    Settings Form Added

---
+++ GreekNakamaTorrentUpload/WinForms/Settings/frmSettings.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace GreekNakamaTorrentUpload.WinForms.Settings
+{
+    partial class frmSettings
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.text_webImageFolder = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btn_openCred = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_apply = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.text_webImageFolder);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(298, 57);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Web Image Folder:";
+            // 
+            // text_webImageFolder
+            // 
+            this.text_webImageFolder.Location = new System.Drawing.Point(6, 19);
+            this.text_webImageFolder.Name = "text_webImageFolder";
+            this.text_webImageFolder.Size = new System.Drawing.Size(286, 20);
+            this.text_webImageFolder.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btn_openCred);
+            this.groupBox2.Location = new System.Drawing.Point(12, 75);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(298, 71);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Open Credentials File:";
+            // 
+            // btn_openCred
+            // 
+            this.btn_openCred.Location = new System.Drawing.Point(91, 26);
+            this.btn_openCred.Name = "btn_openCred";
+            this.btn_openCred.Size = new System.Drawing.Size(125, 23);
+            this.btn_openCred.TabIndex = 0;
+            this.btn_openCred.Text = "Open Credentials";
+            this.btn_openCred.UseVisualStyleBackColor = true;
+            this.btn_openCred.Click += new System.EventHandler(this.btn_openCred_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label1);
+            this.groupBox3.Location = new System.Drawing.Point(316, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(200, 134);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "GUI";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(67, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Not Available";
+            // 
+            // btn_apply
+            // 
+            this.btn_apply.Location = new System.Drawing.Point(441, 161);
+            this.btn_apply.Name = "btn_apply";
+            this.btn_apply.Size = new System.Drawing.Size(75, 23);
+            this.btn_apply.TabIndex = 1;
+            this.btn_apply.Text = "Apply";
+            this.btn_apply.UseVisualStyleBackColor = true;
+            this.btn_apply.Click += new System.EventHandler(this.btn_apply_Click);
+            // 
+            // frmSettings
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(532, 196);
+            this.Controls.Add(this.btn_apply);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "frmSettings";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Settings";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmSettings_FormClosing);
+            this.Load += new System.EventHandler(this.frmSettings_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox text_webImageFolder;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btn_openCred;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_apply;
+    }
+}
---
+++ GreekNakamaTorrentUpload/WinForms/Settings/frmSettings.cs
@@ -0,0 +1,74 @@
+﻿using GreekNakamaTorrentUpload.WinClass.Helper;
+using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace GreekNakamaTorrentUpload.WinForms.Settings
+{
+    public partial class frmSettings : Form
+    {
+        string internetPath;
+        Timer checkIfProcessExited;
+        Process p;
+
+
+        public frmSettings()
+        {
+            InitializeComponent();
+            checkIfProcessExited = new Timer();
+            checkIfProcessExited.Interval = 100; //100 ms
+            checkIfProcessExited.Tick += CheckIfProcessExited_Tick;
+            checkIfProcessExited.Stop();
+            p = new Process();
+
+        }
+
+        private void CheckIfProcessExited_Tick(object sender, EventArgs e)
+        {
+            var HasExited = p.HasExited;
+            if (HasExited)
+            {
+                checkIfProcessExited.Stop();
+                MessageBox.Show("You must restart the program.", "Restart...", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Application.Restart();
+            }
+        }
+
+        private void btn_openCred_Click(object sender, EventArgs e)
+        {
+            DialogResult DL = MessageBox.Show("If you open file. After this you must do restart.\nDo you want to open it?", "Open Credentials...", MessageBoxButtons.OKCancel, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2);
+            if (DL == DialogResult.OK)
+            {
+                p.StartInfo.Arguments = "config.txt";
+                p.StartInfo.FileName = "notepad.exe";
+                p.Start();
+                checkIfProcessExited.Start();
+            }
+        }
+
+        private void btn_apply_Click(object sender, EventArgs e)
+        {
+            internetPath = text_webImageFolder.Text;
+            Helper.SaveToJson(internetPath);
+            MessageBox.Show("Settings has changed. We restart the Application. See you!", "Perfect...", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            Application.Restart();
+        }
+
+        private void frmSettings_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (text_webImageFolder.Text == "")
+            {
+                MessageBox.Show("You don't fill the Web Image Folder Text Box. Please fill it.", "Error...", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                e.Cancel = true;
+            }
+        }
+
+        private void frmSettings_Load(object sender, EventArgs e)
+        {
+            internetPath = Helper.LoadFromJson();
+            if (internetPath == "0x01") return;
+            text_webImageFolder.Text = internetPath;
+
+        }
+    }
+}
