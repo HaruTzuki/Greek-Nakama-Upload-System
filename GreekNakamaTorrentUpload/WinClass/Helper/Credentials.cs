7ce78f8fcd8633f6a578473a90ad6a1f375d63c8
HaruTzuki <HaruTzuki@DESKTOP-32L8FEI>
2017-07-02 00:42:51 +0300

    Credentials Class added.

---
+++ GreekNakamaTorrentUpload/WinClass/Helper/Credentials.cs
@@ -0,0 +1,12 @@
+﻿//This is a class for Credentials to load and save
+
+namespace GreekNakamaTorrentUpload.WinClass.Helper
+{
+    public class Credentials
+    {
+        public string WebImageFolder { get; set; } //Web Destination. Where can program find images
+        public string FTPServer { get; set; } //FTP Server Adress e.g ftp://192.168.1.2
+        public string FTPUsername { get; set; } //FTP Username
+        public string FTPPassword { get; set; } //FTP Password
+    }
+}
