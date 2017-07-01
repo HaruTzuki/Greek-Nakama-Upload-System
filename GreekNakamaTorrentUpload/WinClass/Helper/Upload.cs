ffab211588d637ef846468bc1633e2dab93e20d0
HaruTzuki <HaruTzuki@DESKTOP-32L8FEI>
2017-07-02 00:43:36 +0300

    Upload Class added.

---
+++ GreekNakamaTorrentUpload/WinClass/Helper/Upload.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net;
+using System.Windows.Forms;
+
+namespace GreekNakamaTorrentUpload.WinClass.Helper
+{
+    class Upload
+    {
+        //Varible
+        private string _username;
+        private string _password;
+        private string _path;
+
+        //Constructors
+
+        public Upload() { }
+
+        public Upload(string username, string password, string path)
+        {
+            this._username = username;
+            this._password = password;
+            this._path = path;
+        }
+
+        public void StartUploading(string filePath, string fileName)
+        {
+            try
+            {
+                using (WebClient client = new WebClient())
+                {
+                    client.Credentials = new NetworkCredential(_username, _password);
+                    client.UploadFile("ftp://greek-nakama.com/public_html/blog/torrents/files/" + fileName, "STOR", filePath);
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error while uploading the file. Check your credentials or Internet Connection and try again.", "Error while uploading...", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            }
+        }
+    }
+}
