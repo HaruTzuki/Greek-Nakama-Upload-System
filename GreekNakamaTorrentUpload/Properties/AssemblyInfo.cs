896eec62d1c310001f0a2dd0b13e92b3bd364066
HaruTzuki <HaruTzuki@DESKTOP-32L8FEI>
2017-07-06 20:26:54 +0300

    Assembly info updated.

--- GreekNakamaTorrentUpload/Properties/AssemblyInfo.cs
+++ GreekNakamaTorrentUpload/Properties/AssemblyInfo.cs
@@ -1,15 +1,16 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GreekNakamaTorrentUpload")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Greek-Nakama Upload Tool")]
+[assembly: AssemblyDescription("A tool usefull to upload torrents into server and database")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("GreekNakamaTorrentUpload")]
+[assembly: AssemblyCompany("Greek-Nakama Dev Team")]
+[assembly: AssemblyProduct("Greek-Nakama Upload Tool")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -34,3 +35,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("el")]
+
