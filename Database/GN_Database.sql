d441dd3c6f512671a2d16b2d2398ac975aeb6250
Dimitris Vlachopoulos <dimitris.v.91@hotmail.com>
2017-07-08 22:33:27 +0300

    Commit to Git. Database File.

---
+++ Database/GN_Database.sql
@@ -0,0 +1,50 @@
+/*
+SQLyog Ultimate v12.09 (64 bit)
+MySQL - 10.1.21-MariaDB : Database - download
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`download` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `download`;
+
+/*Table structure for table `gn_anime` */
+
+DROP TABLE IF EXISTS `gn_anime`;
+
+CREATE TABLE `gn_anime` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `anime_name` varchar(255) NOT NULL COMMENT 'The real name of Anime',
+  `anime_slug` varchar(255) NOT NULL COMMENT 'The slug name for images',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `gn_anime` */
+
+/*Table structure for table `tracker` */
+
+DROP TABLE IF EXISTS `tracker`;
+
+CREATE TABLE `tracker` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `downloadlink` varchar(255) DEFAULT NULL,
+  `imageLink` varchar(255) DEFAULT NULL,
+  `uploadDate` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `tracker` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
